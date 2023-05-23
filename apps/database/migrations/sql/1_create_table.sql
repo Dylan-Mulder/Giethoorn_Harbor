75862d93fb1a84fd4821d2f1cc254462b0168d32
Luc Joosten <120277042+lhajoosten@users.noreply.github.com>
2023-05-23 13:09:44 +0200

    Postgress docker draait goed

--- apps/database/migrations/sql/1_create_table.sql
+++
@@ -1,4 +0,0 @@
-CREATE TABLE user_profile (
-  user_id VARCHAR(64) PRIMARY KEY,
-  name VARCHAR(256)
-);
--- apps/solarch/src/main.ts
+++ apps/solarch/src/main.ts
@@ -1,12 +1,9 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import database from '../../database/main'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
-  await app.listen(5000, () => {
-    database.runMigrations();
-  });
+  await app.listen(5000);
 }
 bootstrap();
