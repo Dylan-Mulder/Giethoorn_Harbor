d94b05afcd2b5643e05583695376f1f162012dfe
Luc Joosten <120277042+lhajoosten@users.noreply.github.com>
2023-05-23 12:15:35 +0200

    postgres bijna ingebouwd

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,4 @@
-module.exports = {
+atamodule.exports = {
   parser: '@typescript-eslint/parser',
   parserOptions: {
     project: 'tsconfig.json',
--- apps/solarch/src/main.ts
+++ apps/solarch/src/main.ts
@@ -1,9 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import database from '../../database/main'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
-  await app.listen(5000);
+  await app.listen(5000, () => {
+    database.runMigrations();
+  });
 }
 bootstrap();
