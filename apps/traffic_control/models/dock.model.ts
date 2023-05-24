7ece075c961768dcab515d14f40f0e8ac1b24015
Luc Joosten <120277042+lhajoosten@users.noreply.github.com>
2023-05-24 11:46:24 +0200

    added models Trafic Control

---
+++ apps/traffic_control/models/dock.model.ts
@@ -0,0 +1,7 @@
+export class Dock {
+  id: number;
+  name: string;
+  description: string;
+  amountOfShipSpots: number;
+  amountOfTruckSpots: number;
+}
---
+++ apps/traffic_control/models/tugboat.model.ts
@@ -0,0 +1,4 @@
+export class Tugboat {
+  id: number;
+  name: string;
+}
