669326b5e5d0d0cf75880017550ae252304fd98b
Tivawo <twouwe@avans.nl>
2023-05-25 17:24:05 +0200

    I wanna die

--- relational_database_init.sql
+++ relational_database_init.sql
@@ -1,6 +1,9 @@
-CREATE USER test WITH PASSWORD 'test' CREATEDB;
-CREATE DATABASE test
-    WITH 
-    OWNER = test
-    ENCODING = 'UTF8'
-    TABLESPACE = pg_default
+-- USERS
+CREATE USER testuser WITH PASSWORD 'jw8s0F4';
+
+-- DATABASES
+CREATE DATABASE test OWNER testuser
+    CREATE SCHEMA hollywood
+        CREATE TABLE films (title text, release date, awards text[])
+        CREATE VIEW winners AS
+            SELECT title, release FROM films WHERE awards IS NOT NULL;
