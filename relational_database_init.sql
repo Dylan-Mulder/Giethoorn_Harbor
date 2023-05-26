1dba0cfeb81a3c1045647539d40854a27af76203
Tivawo <tvw203@gmail.com>
2023-05-26 11:55:52 +0200

    SQL Update

--- relational_database_init.sql
+++ relational_database_init.sql
@@ -2,8 +2,9 @@
 CREATE USER testuser WITH PASSWORD 'jw8s0F4';
 
 -- DATABASES
-CREATE DATABASE test OWNER testuser
-    CREATE SCHEMA hollywood
-        CREATE TABLE films (title text, release date, awards text[])
-        CREATE VIEW winners AS
-            SELECT title, release FROM films WHERE awards IS NOT NULL;
+CREATE DATABASE test OWNER testuser;
+
+CREATE SCHEMA hollywood
+    CREATE TABLE films (title text, release date, awards text[])
+    CREATE VIEW winners AS
+        SELECT title, release FROM films WHERE awards IS NOT NULL;
