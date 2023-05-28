272bcf1191c31a594d1959d7ecd150001359ae20
Tivawo <tvw203@gmail.com>
2023-05-28 11:12:51 +0200

    WIP in progress

--- database init scripts/relational_database_init.sql
+++ database init scripts/relational_database_init.sql
@@ -26,6 +26,7 @@ CREATE SCHEMA messaging;
 CREATE TABLE traffic_control.traffic_planning
 (
     id serial NOT NULL,
+    passages jsonb NOT NULL,
     start_date date NOT NULL,
     end_date date NOT NULL,
     PRIMARY KEY (id)
@@ -33,10 +34,10 @@ CREATE TABLE traffic_control.traffic_planning
 CREATE TABLE traffic_control.passage
 (
     id serial NOT NULL,
-    traffic_planning_id integer NOT NULL,
     dock_id integer NOT NULL,
     ship_id integer,
     truck_id integer,
+    tugboats jsonb NOT NULL,
     arrival timestamp without time zone NOT NULL,
     departure timestamp without time zone NOT NULL,
     PRIMARY KEY (id)
@@ -61,7 +62,7 @@ CREATE TABLE traffic_control.dock
 (
     id serial NOT NULL,
     name text NOT NULL,
-    description text DEFAULT No description provided,
+    description text DEFAULT 'No description provided',
     amount_of_ship_spots integer DEFAULT 1,
     amount_of_truck_spots integer DEFAULT 3,
     PRIMARY KEY (id)
@@ -76,7 +77,10 @@ CREATE TABLE traffic_control.tugboat
 ALTER TABLE IF EXISTS traffic_control.traffic_planning
     OWNER to gh_traffic_control;
 ALTER TABLE IF EXISTS traffic_control.passage
-    OWNER to gh_traffic_control;
+    OWNER to gh_traffic_control,
+    ADD CONSTRAINT fk_dock FOREIGN KEY(dock_id) REFERENCES traffic_control.dock(id),
+    ADD CONSTRAINT fk_ship FOREIGN KEY(ship_id) REFERENCES traffic_control.ship(id),
+    ADD CONSTRAINT fk_truck FOREIGN KEY(truck_id) REFERENCES traffic_control.truck(id);
 ALTER TABLE IF EXISTS traffic_control.ship
     OWNER to gh_traffic_control;
 ALTER TABLE IF EXISTS traffic_control.truck
@@ -85,6 +89,42 @@ ALTER TABLE IF EXISTS traffic_control.dock
     OWNER to gh_traffic_control;
 ALTER TABLE IF EXISTS traffic_control.tugboat
     OWNER to gh_traffic_control;
+
+-- Dock Rental
+CREATE TABLE dock_rental.dock
+(
+    id serial NOT NULL,
+    name text NOT NULL,
+    PRIMARY KEY (id)
+);
+CREATE TABLE dock_rental.shipping_company
+(
+    id serial NOT NULL,
+    name text NOT NULL,
+    kvk_number integer NOT NULL,
+    country text NOT NULL,
+    PRIMARY KEY (id)
+);
+CREATE TABLE dock_rental.lease_agreement
+(
+    id serial NOT NULL,
+    uuid uuid NOT NULL,
+    dock_id integer NOT NULL,
+    shipping_company_id integer NOT NULL,
+    sign_date date NOT NULL,
+    valid_until date NOT NULL,
+    price numeric(18, 2) NOT NULL,
+    PRIMARY KEY (id)
+);
+
+ALTER TABLE IF EXISTS dock_rental.dock
+    OWNER to gh_dock_rental;
+ALTER TABLE IF EXISTS dock_rental.shipping_company
+    OWNER to gh_dock_rental;
+ALTER TABLE IF EXISTS dock_rental.lease_agreement
+    OWNER to gh_dock_rental,
+    ADD CONSTRAINT fk_dock FOREIGN KEY(dock_id) REFERENCES dock_rental.dock(id),
+    ADD CONSTRAINT fk_shipping_company FOREIGN KEY(shipping_company_id) REFERENCES dock_rental.shipping_company(id);
 
 -- Billing
 CREATE TABLE billing.ship_service
