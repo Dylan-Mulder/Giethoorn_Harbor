3c348ca8e9d8d2c3c76a46c6e582c6115f3d24d7
jeremysmits <jeremysmts@gmail.com>
2023-05-29 10:35:26 +0200

    added config

--- apps/refilling/src/refilling.module.ts
+++ apps/refilling/src/refilling.module.ts
@@ -1,26 +1,14 @@
 import { Module } from '@nestjs/common';
-import { RefillingController } from './refilling.controller';
-import { RefillingService } from './refilling.service';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { configService } from './config/config.service';
+import { RefillingController } from './refilling.controller';
 
 @Module({
   imports: [
-    ConfigModule.forRoot({ isGlobal: true, envFilePath: './.env' }),
-    TypeOrmModule.forRootAsync({
-      imports: [ConfigModule],
-      useFactory: (configService: ConfigService) => ({
-        "type": "postgres",
-        "host": "172.19.0.2",
-        "port": 5432,
-        "username": "postgres",
-        "password": "password",
-        "database": "postgres",
-        //"entities": ["./**/*.model.ts"],
-        "synchronize": true
-      }),
-      inject: [ConfigService]
-    }),
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRoot(configService.getTypeOrmConfig()),
   ],
+  controllers: [RefillingController]
 })
-export class RefillingModule {}
+export class RefillingModule { }
